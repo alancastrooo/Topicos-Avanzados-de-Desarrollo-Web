8f1bfcb13210ae2a8c6182b21a053d3e4a97ae6b
Alan Castro <alanarturocastrooo@gmail.com>
2025-10-07 09:24:06 -0600

    Vhanged it to mongoose

--- Unidad II/gestion-eventos-backend/app.js
+++ Unidad II/gestion-eventos-backend/app.js
@@ -1,11 +1,10 @@
-import { connectMDB } from "./config/mongoDB.js";
 import app from "./config/server.js"
 import dotenv from 'dotenv';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
-await connectMDB()
+
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`)
